dd28bced3f39805d67d26cf8be34e5fdc18c244c
maryzvrz <zavarzaevam@gmail.com>
2023-12-20 22:43:19 +1000

    Add singleton task

--- src/03_Singleton/singleton.py
+++ src/03_Singleton/singleton.py
@@ -1,2 +1,7 @@
 class Singleton:
-    pass
+    instance = None
+
+    def __new__(cls):
+        if Singleton.instance is None:
+            Singleton.instance = super(Singleton, cls).__new__(cls)
+        return Singleton.instance
