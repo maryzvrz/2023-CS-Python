257c21c395719f56086079ef40bc42741c2b334d
maryzvrz <zavarzaevam@gmail.com>
2024-01-02 22:40:08 +1000

    Add ocean task

--- src/04_Ocean/ocean.py
+++ src/04_Ocean/ocean.py
@@ -1,32 +1,54 @@
-import sys
-from typing import List
+import numpy as np
+
+pole = np.matrix(
+    [[3, 3, 3, 2, 1], [1, 0, 1, 1, 1], [2, 2, 2, 2, 2], [3, 3, 3, 3, 3], [3, 2, 1, 2, 3]]
+)
+
+width, length = pole.shape
+nraz = 1
+raz = 0
+two = 2
+three = 3
 
 
-class Ocean:
-    state: List[List[int]]
+def neight(pole):
+    neigh = []
+    if i > 0 and j > 0:
+        neigh.append(pole[i - 1, j - 1])
+    if i > 0:
+        neigh.append(pole[i - 1, j])
+    if i > 0 and j < length - 1:
+        neigh.append(pole[i - 1, j + 1])
+    if j > 0:
+        neigh.append(pole[i, j - 1])
+    if j < length - 1:
+        neigh.append(pole[i, j + 1])
+    if i < width - 1 and j > 0:
+        neigh.append(pole[i + 1, j - 1])
+    if i < width - 1:
+        neigh.append(pole[i + 1, j])
+    if i < width - 1 and j < length - 1:
+        neigh.append(pole[i + 1, j + 1])
+    return str(neigh)
 
-    def __init__(self, init_state: List[List[int]]):
-        self.state = init_state
 
-    def __str__(self) -> str:
-        return "\n".join(["".join(str(el) for el in row) for row in self.state])
+for _ in range(nraz):
+    for i in range(width):
+        for j in range(length):
+            fish = neight(pole).count("2")
+            shrimp = neight(pole).count("3")
+            if pole[i, j] == 0:
+                if fish == three:
+                    pole[i, j] = 2
+                if shrimp == three:
+                    pole[i, j] = 3
+                if fish == three and shrimp == three:
+                    pole[i, j] = 2
+            if pole[i, j] == two:
+                if fish < two or fish > three:
+                    pole[i, j] = 0
+            if pole[i, j] == three:
+                if shrimp < two or shrimp > three:
+                    pole[i, j] = 0
 
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}({self.state!r})"
-
-    def gen_next_quantum(self) -> "Ocean":
-        raise NotImplementedError
-
-
-if __name__ == "__main__":
-    n_quantums = int(sys.stdin.readline())
-    n_rows, n_clms = (int(i) for i in sys.stdin.readline().split())
-    init_state = []
-    for i in range(n_rows):
-        line = [int(i) for i in sys.stdin.readline().split()]
-        init_state.append(line)
-
-    ocean = Ocean(init_state=init_state)
-    for _ in range(n_quantums):
-        ocean = ocean.gen_next_quantum()
-    print(ocean)
+print(pole)
